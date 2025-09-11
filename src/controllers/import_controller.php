8dacf3f932cb92e06665d209b148300a28f7fc47
ocelot73 <59670568+ocelot73@users.noreply.github.com>
2025-09-11 06:16:57 +0200

    Create import_controller.php

---
+++ src/controllers/import_controller.php
@@ -0,0 +1,10 @@
+<?php
+// /src/controllers/import_controller.php
+
+/**
+ * Prepara i dati per la pagina di importazione.
+ * In questo caso non ci sono dati da caricare dal DB.
+ */
+function import_data($conn, $pageConfig) {
+    return []; // La pagina è gestita principalmente da JS
+}
