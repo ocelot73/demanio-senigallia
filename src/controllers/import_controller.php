68feb208814ce32a71a1484bc75784b4b253add4
ocelot73 <59670568+ocelot73@users.noreply.github.com>
2025-09-12 16:07:34 +0200

    Update import_controller.php

--- src/controllers/import_controller.php
+++ src/controllers/import_controller.php
@@ -5,6 +5,6 @@
  * Prepara i dati per la pagina di importazione.
  * In questo caso non ci sono dati da caricare dal DB.
  */
-function import_data($conn, $pageConfig) {
-    return []; // La pagina è gestita principalmente da JS
+function importa_controller_data($conn, $pageConfig) {
+    return []; // La pagina è gestita principalmente da JS e dalla logica SSE in index.php
 }
