f6e96a8cf4b7cdf95e781edfaa6160a50f8709a8
ocelot73 <59670568+ocelot73@users.noreply.github.com>
2025-09-12 08:17:00 +0200

    Update pagination.php

--- templates/partials/pagination.php
+++ templates/partials/pagination.php
@@ -2,19 +2,22 @@
 if (isset($total_pages) && $total_pages > 1): ?>
 <div class="pagination">
     <?php if ($current_page > 1): ?>
-      <a href="?page=<?= $currentPageKey ?>&p=1">&laquo;</a>
-      <a href="?page=<?= $currentPageKey ?>&p=<?= $current_page - 1 ?>">&lsaquo;</a>
+      <a href="<?= build_current_url(['p' => 1]) ?>">&laquo;</a>
+      <a href="<?= build_current_url(['p' => $current_page - 1]) ?>">&lsaquo;</a>
     <?php else: ?>
       <span class="disabled">&laquo;</span><span class="disabled">&lsaquo;</span>
     <?php endif; ?>
     
-    <?php for ($i = max(1, $current_page - 2); $i <= min($total_pages, $current_page + 2); $i++): ?>
-        <a href="?page=<?= $currentPageKey ?>&p=<?= $i ?>" class="<?= $i == $current_page ? 'current' : '' ?>"><?= $i ?></a>
-    <?php endfor; ?>
+    <?php 
+      $start = max(1, $current_page - 2); $end = min($total_pages, $current_page + 2);
+      if ($start > 1) { echo '<a href="' . build_current_url(['p' => 1]) . '">1</a>'; if ($start > 2) echo '<span class="dots">...</span>'; }
+      for ($i = $start; $i <= $end; $i++) echo $i == $current_page ? '<span class="current">' . $i . '</span>' : '<a href="' . build_current_url(['p' => $i]) . '">' . $i . '</a>';
+      if ($end < $total_pages) { if ($end < $total_pages - 1) echo '<span class="dots">...</span>'; echo '<a href="' . build_current_url(['p' => $total_pages]) . '">' . $total_pages . '</a>'; }
+    ?>
 
     <?php if ($current_page < $total_pages): ?>
-      <a href="?page=<?= $currentPageKey ?>&p=<?= $current_page + 1 ?>">&rsaquo;</a>
-      <a href="?page=<?= $currentPageKey ?>&p=<?= $total_pages ?>">&raquo;</a>
+      <a href="<?= build_current_url(['p' => $current_page + 1]) ?>">&rsaquo;</a>
+      <a href="<?= build_current_url(['p' => $total_pages]) ?>">&raquo;</a>
     <?php else: ?>
       <span class="disabled">&rsaquo;</span><span class="disabled">&raquo;</span>
     <?php endif; ?>
