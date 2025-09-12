e4b40ba78fe2b6c11a26702b40cdf4189ac2fabe
ocelot73 <59670568+ocelot73@users.noreply.github.com>
2025-09-12 08:59:39 +0200

    Update pagination.php

--- templates/partials/pagination.php
+++ templates/partials/pagination.php
@@ -1,24 +1,25 @@
-<?php // /templates/partials/pagination.php
-
-// Salvaguardie
-$total_pages  = isset($total_pages)  ? max(1, (int)$total_pages) : 1;
-$current_page = isset($current_page) ? max(1, (int)$current_page) : max(1, (int)($_GET['p'] ?? 1));
-
-$go = function(int $p) {
-    return htmlspecialchars(build_current_url(['p' => $p]));
-};
-
-$prev = max(1, $current_page - 1);
-$next = min($total_pages, $current_page + 1);
-?>
+<?php // /templates/partials/pagination.php 
+if (isset($total_pages) && $total_pages > 1): ?>
 <div class="pagination">
-    <a class="nav-button <?= $current_page <= 1 ? 'disabled' : '' ?>" href="<?= $go(1) ?>">&laquo;</a>
-    <a class="nav-button <?= $current_page <= 1 ? 'disabled' : '' ?>" href="<?= $go($prev) ?>">&lsaquo;</a>
+    <?php if ($current_page > 1): ?>
+      <a href="<?= build_current_url(['p' => 1]) ?>">&laquo;</a>
+      <a href="<?= build_current_url(['p' => $current_page - 1]) ?>">&lsaquo;</a>
+    <?php else: ?>
+      <span class="disabled">&laquo;</span><span class="disabled">&lsaquo;</span>
+    <?php endif; ?>
+    
+    <?php 
+      $start = max(1, $current_page - 2); $end = min($total_pages, $current_page + 2);
+      if ($start > 1) { echo '<a href="' . build_current_url(['p' => 1]) . '">1</a>'; if ($start > 2) echo '<span class="dots">...</span>'; }
+      for ($i = $start; $i <= $end; $i++) echo $i == $current_page ? '<span class="current">' . $i . '</span>' : '<a href="' . build_current_url(['p' => $i]) . '">' . $i . '</a>';
+      if ($end < $total_pages) { if ($end < $total_pages - 1) echo '<span class="dots">...</span>'; echo '<a href="' . build_current_url(['p' => $total_pages]) . '">' . $total_pages . '</a>'; }
+    ?>
 
-    <?php for ($i = 1; $i <= $total_pages; $i++): ?>
-        <a class="nav-button <?= $i === $current_page ? 'active' : '' ?>" href="<?= $go($i) ?>"><?= $i ?></a>
-    <?php endfor; ?>
-
-    <a class="nav-button <?= $current_page >= $total_pages ? 'disabled' : '' ?>" href="<?= $go($next) ?>">&rsaquo;</a>
-    <a class="nav-button <?= $current_page >= $total_pages ? 'disabled' : '' ?>" href="<?= $go($total_pages) ?>">&raquo;</a>
+    <?php if ($current_page < $total_pages): ?>
+      <a href="<?= build_current_url(['p' => $current_page + 1]) ?>">&rsaquo;</a>
+      <a href="<?= build_current_url(['p' => $total_pages]) ?>">&raquo;</a>
+    <?php else: ?>
+      <span class="disabled">&rsaquo;</span><span class="disabled">&raquo;</span>
+    <?php endif; ?>
 </div>
+<?php endif; ?>
