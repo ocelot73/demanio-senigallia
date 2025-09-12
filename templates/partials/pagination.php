5c6c942b6beebe59f5e6f121fadb8389827548e8
ocelot73 <59670568+ocelot73@users.noreply.github.com>
2025-09-12 22:56:27 +0200

    Update pagination.php

--- templates/partials/pagination.php
+++ templates/partials/pagination.php
@@ -7,8 +7,9 @@ $current = (int)($current_page ?? 1);
 ?>
 <div class="pagination">
     <?php if ($current > 1): ?>
-      <a href="<?= htmlspecialchars(build_current_url(['p' => 1])) ?>">&laquo; Prima</a>
-      <a href="<?= htmlspecialchars(build_current_url(['p' => $current - 1])) ?>">&lsaquo; Prec.</a>
+      <!-- CORREZIONE: Usa 'page' invece di 'p' per il parametro -->
+      <a href="<?= htmlspecialchars(build_current_url(['page' => 1])) ?>">&laquo; Prima</a>
+      <a href="<?= htmlspecialchars(build_current_url(['page' => $current - 1])) ?>">&lsaquo; Prec.</a>
     <?php else: ?>
       <span class="disabled">&laquo; Prima</span><span class="disabled">&lsaquo; Prec.</span>
     <?php endif; ?>
@@ -16,43 +17,31 @@ $current = (int)($current_page ?? 1);
     <?php
       $start = max(1, $current - 2);
       $end = min($total_pages, $current + 2);
-      
-      if ($start > 1) { 
-          echo '<a href="' . htmlspecialchars(build_current_url(['p' => 1])) . '">1</a>';
-          if ($start > 2) echo '<span class="disabled">...</span>'; 
+
+      if ($start > 1) {
+          echo '<a href="' . htmlspecialchars(build_current_url(['page' => 1])) . '">1</a>';
+          if ($start > 2) echo '<span class="disabled">...</span>';
       }
-      
+
       for ($i = $start; $i <= $end; $i++) {
-          echo $i == $current 
-            ? '<span class="current">' . $i . '</span>' 
-            : '<a href="' . htmlspecialchars(build_current_url(['p' => $i])) . '">' . $i . '</a>';
+          echo $i == $current
+            ? '<span class="current">' . $i . '</span>'
+            : '<a href="' . htmlspecialchars(build_current_url(['page' => $i])) . '">' . $i . '</a>';
       }
-      
-      if ($end < $total_pages) { 
+
+      if ($end < $total_pages) {
           if ($end < $total_pages - 1) echo '<span class="disabled">...</span>';
-          echo '<a href="' . htmlspecialchars(build_current_url(['p' => $total_pages])) . '">' . $total_pages . '</a>';
+          echo '<a href="' . htmlspecialchars(build_current_url(['page' => $total_pages])) . '">' . $total_pages . '</a>';
       }
     ?>
-    
+
     <?php if ($current < $total_pages): ?>
-      <a href="<?= htmlspecialchars(build_current_url(['p' => $current + 1])) ?>">Succ. &rsaquo;</a>
-      <a href="<?= htmlspecialchars(build_current_url(['p' => $total_pages])) ?>">Ultima &raquo;</a>
+      <a href="<?= htmlspecialchars(build_current_url(['page' => $current + 1])) ?>">Succ. &rsaquo;</a>
+      <a href="<?= htmlspecialchars(build_current_url(['page' => $total_pages])) ?>">Ultima &raquo;</a>
     <?php else: ?>
       <span class="disabled">Succ. &rsaquo;</span>
       <span class="disabled">Ultima &raquo;</span>
     <?php endif; ?>
-    
+
     <span style="margin-left:10px; color: var(--color-text-secondary);">Pagina <?= $current ?> di <?= $total_pages ?></span>
-</div>
-    ?>
-    
-    <?php if ($current < $total_pages): ?>
-      <a href="<?= htmlspecialchars(build_current_url(['p' => $current + 1])) ?>">Succ. &rsaquo;</a>
-      <a href="<?= htmlspecialchars(build_current_url(['p' => $total_pages])) ?>">Ultima &raquo;</a>
-    <?php else: ?>
-      <span class="disabled">Succ. &rsaquo;</span>
-      <span class="disabled">Ultima &raquo;</span>
-    <?php endif; ?>
-    
-    <span style="margin-left:10px; color: var(--color-text-secondary); font-size: 0.85rem; font-weight: 600;">Pagina <?= $current ?> di <?= $total_pages ?> (Tot: <?= $total_records ?>)</span>
 </div>
